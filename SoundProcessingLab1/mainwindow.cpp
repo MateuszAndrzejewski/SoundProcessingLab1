eadbafdbe1d389691bfb8755086c3d93eb756329
Mateusz <yamsza1991@o2.pl>
2014-03-15 17:51:57 +0100

    Open file, play .wav file
    - created a button invoking open file dialog in which user chooses the
    file
    - created a button resposible for starting the previously chosen .wav
    file

--- SoundProcessingLab1/mainwindow.cpp
+++ SoundProcessingLab1/mainwindow.cpp
@@ -1,6 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QtMultimedia/QSound>
+#include <QString>
+
+QString fileName;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +18,16 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
+                                                    QStandardPaths::standardLocations(QStandardPaths::DocumentsLocation).at(0),
+                                                    tr("Sound files (*.wav);;All files (*.*)"));
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QSound::play(fileName);
 }
